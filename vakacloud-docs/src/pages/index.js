d940fb5ce37d0f67f1a49355a7087ec50b85edd8
RanHinen <rasmus@vakacloud.fi>
2025-12-22 08:39:27 +0200

    Minor update

--- vakacloud-docs/src/pages/index.js
+++ vakacloud-docs/src/pages/index.js
@@ -4,7 +4,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
-import Translate from '@docusaurus/Translate'; // <--- TÄMÄ ON TÄRKEÄ TUONTI
+import Translate from '@docusaurus/Translate'; 
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -24,17 +24,17 @@ function HomepageHeader() {
           <Link
             className="button button--primary button--lg"
             to="/blog/tags/news">
-            <Translate>Uutiset 📰</Translate>
+            <Translate>Uutiset</Translate>
           </Link>
           <Link
             className="button button--info button--lg"
             to="/blog/tags/updates">
-            <Translate>Päivitykset 🚀</Translate>
+            <Translate>Päivitykset</Translate>
           </Link>
           <Link
             className="button button--warning button--lg"
             to="/blog/tags/maintenance">
-            <Translate>Huoltoilmoitukset ⚠️</Translate>
+            <Translate>Huoltoilmoitukset</Translate>
           </Link>
         </div>
       </div>
